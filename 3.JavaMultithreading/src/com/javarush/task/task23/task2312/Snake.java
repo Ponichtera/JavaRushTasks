635e7bc742707f4871181e190999d719c75256f3
Ponichtera <mcin@ymail.com>
2017-11-01 17:21:06 +0100

    Working on Snake game

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,4 +1,43 @@
 package com.javarush.task.task23.task2312;
 
+import sun.swing.SwingUtilities2;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snake {
+    private boolean isAlive;
+    private List<SnakeSection> sections = new ArrayList<>();
+    private SnakeDirection direction;
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public Snake (int x, int y) {
+        SnakeSection head = new SnakeSection(x,y);
+        sections.add(head);
+        isAlive = true;
+
+    }
+    public int getX() {
+        return sections.get(0).getX();
+    }
+    public int getY() {
+        return sections.get(0).getY();
+    }
+
+    public void move() {}
 }
